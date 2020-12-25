8f4be6bd201dd5018afe9a464cf21b9c5a3df528
Jonathan MARTIN <j.martin@lonsdale.fr>
2020-12-25 14:18:02 +0100

    base

---
+++ README.md
@@ -0,0 +1,112 @@
+
+# Filter Ajax
+
+![version](https://img.shields.io/github/manifest-json/v/Natjo/formValidate)
+
+Ajax form filter with more button.  
+Set current page and items per page in data attibutes.  
+if data-items-per-page = 8 and 16 items are in DOM, set data-current-page="2"  
+
+Need this 3 elements:  
+- `namespace`-form - form with checkboxes or radio
+- `namespace`-result - element wher items will be displayed
+- `namespace`-more - add items until max_page
+
+See [formValidate](https://github.com/Natjo/formValidate) to add native validation. 
+
+## form filter
+```html
+	<form action="myAction" class="filterAjax-form" data-nonce="myNonce" novalidate="novalidate" role="form" aria-label="Search filter" data-current-page="1" data-items-per-page="4">
+
+    <fieldset class="category">
+        <legend>Category</legend>
+
+        <div class="field">
+            <input type="radio" id="choice-0" name="cat[]" value="choice-0">
+			<label for="choice-0">Choice 0</label>
+        </div>
+
+        <div class="field">
+            <input type="radio" id="choice-1" name="cat[]" value="choice-1">
+			<label for="choice-1">Choice 1</label>
+        </div>
+
+    </fieldset>
+
+	<div class="action">
+		<button type="reset">Reset filters</button>
+		<button type="button" class="btn-filter">Filter</button>
+	</div>
+</form>
+```
+## result
+```html
+<ul class="filterAjax-result">
+	<li>
+		<div class="card">card 1</div>
+	</li>
+	<li>
+		<div class="card">card 1</div>
+	</li>
+		<li>
+		<div class="card">card 1</div>
+	</li>
+		<li>
+		<div class="card">card 1</div>
+	</li>
+</ul>
+<button class="filterAjax-more">More result</button>
+```
+## template
+```html
+<template id="tpl-card">
+	<li class="card">
+		<a href="${data.url}">
+			<div class="tag">${data.tag}</div>
+			<h1>${data.title}</h1>
+			<div class="desc">${data.desc}</div>
+		</a>
+	</li>
+</template>
+```
+
+## javscript
+```javascript
+const myfilter = new FilterAjax({
+    //namespace: 'filterAjax',
+    onload(){
+    }
+});
+
+// init filter with url get params on setting
+// checkboxes and radio checked to true and myfilter.load();
+```
+
+## php
+```php
+add_action( 'wp_ajax_mmyAction', 'mmyAction_callback' );
+add_action( 'wp_ajax_nopriv_mmyAction', 'mmyAction_callback' );
+
+function myAction_callback()
+{
+    $response['status'] = 200;
+	$response['page'] = 1;
+	$response['max_page'] = 2;
+	$response['msg'] = __('No result', 'lsd_lang');
+	$response['msg'] = array(
+		array(
+			"tag" => "amet",
+			"title" => "Lorem ipsum",
+			"desc" => "Lorem ipsum dolor sit amet consectetur, adipisicing elit.",
+			"url" => "/",
+		)
+	);
+
+    wp_send_json($response);
+}
+```
+
+## Demo
+[See codepen demo](https://codepen.io/natjo/pen/LYRzwXB?editors=1011)
+
+
---
+++ filterAjax.js
@@ -0,0 +1,98 @@
+/**
+ * Ajax form filter with more button
+ * Set current page and items per page: 
+ * if data-items-per-page = 8 and 16 items are in DOM set  data-current-page="2"
+ * Need this 3 elements:
+ * 
+ * namespace-form - form with checkboxes or radio
+ * namespace-result - element wher items will be displayed
+ * namespace-more - add items until max_page
+ * 
+ * @param {Object} params
+ * @param {string} params.namespace default is filterAjax
+ * 
+ */
+
+function FilterAjax(params) {
+	let page = form.dataset.currentPage || 1;
+    let items_per_page = form.dataset.itemsPerPage || 8;
+    const namespace = params.namespace || 'filterAjax';
+    const form = document.querySelector(`.${namespace}-form`);
+    const btn_reset = form.querySelector('[type="reset"]');
+    const btn_filter = form.querySelector('.btn-filter');
+    const result = document.querySelector(`.${namespace}-result`);
+    const btn_more = document.querySelector(`.${namespace}-more`);
+	const tpl = document.getElementById(`${namespace}-tpl`).innerHTML;
+
+    const clear = () => result.innerHTML = '';
+
+    const loading = () => {
+		form.classList.add('loading');
+		btn_more.classList.add('loading');
+		btn_more.disabled = true;
+	};
+
+    const onload = () => {
+		form.classList.remove('loading');
+        btn_more.classList.remove('loading');
+		btn_more.disabled = false;
+	};
+	
+    const reset = () => {
+		clear();
+		page = 1;
+		form.querySelectorAll('input:checked').forEach(item => (item.checked = false));
+		ajax();
+	};
+
+    const ajax = () => {
+        loading();
+
+        const formData = new FormData(form);
+        formData.append('nonce', form.dataset.nonce);
+        formData.append('action', form.action);
+        formData.append('items_per_page', items_per_page);
+		formData.append('page', page);
+
+       	const xhr = new XMLHttpRequest();
+        xhr.open('POST', url, true);
+        xhr.onreadystatechange = () => {
+			if (xhr.readyState === 4 && (xhr.status === 200 || xhr.status === 0)) {
+                const response = JSON.parse(xhr.response);
+				page == 1 && clear();
+				if (response.datas.length) {
+                   for (let data of response.datas) {
+						const card = eval("`" + tpl + "`");
+						result.insertAdjacentHTML('beforeend', card);
+					} 
+					btn_more.style.display = page >= response.max_page ? 'none' : 'block';
+                } else{
+					result.innerHTML = response.msg;
+					btn_more.style.display = 'none';
+				}
+            }
+            onload();
+            if(typeof params.onload === 'function') params.onload(response);
+        };
+
+        xhr.send(formData);
+    };
+
+    form.querySelectorAll('input:checked').forEach(item => (item.checked = false));
+
+	btn_filter.onclick = () => ajax();
+
+    btn_more.onclick = () => {
+		page++;
+		ajax();
+	};
+	
+    btn_reset.onclick = () => reset();
+	
+	this.load = () => {
+		page = 1;
+		ajax();
+	}
+}
+
+export default FilterAjax;
